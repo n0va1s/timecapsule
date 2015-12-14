bd547a52bd3de9f0413f54516e063abc8f3ef6d7
João Paulo Novais <joaopaulonovais@gmail.com>
2015-12-14 20:41:42 -0200

    Primeira versao dos scripts de criacao e delecao do banco de dados MySQL

---
+++ database/timecapsule_create.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE capsule;
+
+CREATE USER 'usrcapsule'@'localhost' IDENTIFIED BY 'capsuleusr';
+GRANT ALL PRIVILEGES ON capsule. * TO 'usrcapsule'@'localhost';
+FLUSH PRIVILEGES;
+
+USE capsule;
+
+CREATE TABLE IF NOT EXISTS user (
+seq_user INT(6) AUTO_INCREMENT PRIMARY KEY,
+nam_user VARCHAR(50) NOT NULL DEFAULT '',
+pwd_user VARCHAR(30) DEFAULT '',
+eml_user VARCHAR(50) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE IF NOT EXISTS message (
+seq_message INT(6) AUTO_INCREMENT PRIMARY KEY,
+dat_message DATE NOT NULL DEFAULT '0000-00-00',
+eml_message VARCHAR(255) NOT NULL DEFAULT '',
+tel_message VARCHAR (15) DEFAULT '',
+dat_log TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+seq_user INT(6) NOT NULL
+);
+
+ALTER TABLE message ADD FOREIGN KEY (seq_user) REFERENCES user(seq_user);
