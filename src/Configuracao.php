6013a4588c16d926a51101a992a99eaa8bab819d
João Paulo Novais <joaopaulonovais@gmail.com>
2016-02-24 00:03:55 -0300

    Ajustes de configuração

---
+++ src/Configuracao.php
@@ -0,0 +1,14 @@
+<?php
+class Configuracao {
+	public $SGBD = 'mysql';
+	public $banco = 'capsdtem_capsule';
+	public $charset = 'utf8';
+	//Desenvolvimento
+	public $usuario_des = 'usrcapsule';
+	public $senha_des = 'capsuleusr';
+	public $servidor_des = 'localhost';
+	//Producao
+	public $usuario_prd = 'capsdtem_usrcaps';
+	public $senha_prd = '(J+9O1??yK$A';
+	public $servidor_prd = 'localhost';
+}
